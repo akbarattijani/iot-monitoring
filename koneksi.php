a2de31bcf1a0d9876bab4f6369dcd8f58829eacc
dicky.akbar@dwidasa.com <akbar128256512>
2020-02-08 15:08:35 +0700

    ganti ke postgres

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,4 @@
 <?php
-//Mendefinisikan Konstanta
-define('HOST','localhost');
-define('USER','root');
-define('PASS','');
-define('DB','keamanan_kendaraan');
 //membuat koneksi dengan database
-$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+$con = pg_connect("host=ec2-54-80-184-43.compute-1.amazonaws.com dbname=d2ke05v2qpjs6p user=svzmsffukddwhs password=b1866be2ae7d3b16a56bd628eb175219b8c93adb64be6e8f1d26fbb1e1e57b0a");
 ?>
