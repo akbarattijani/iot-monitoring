eaf266c194106ba0f10d228150131ced0094610f
dicky.akbar@dwidasa.com <akbar128256512>
2020-02-08 18:14:21 +0700

    com

--- InsertAlarm.php
+++ InsertAlarm.php
@@ -1,27 +1,38 @@
 <?php 
-	if($_SERVER['REQUEST_METHOD']=='POST')
-	{
-		//Mendapatkan Nilai Variable
-		date_default_timezone_set('Asia/Jakarta');
-		
-		$ControlNameAlarm = $_POST['ControlNameAlarm'];
-		$ControlStatusAlarm = $_POST['ControlStatusAlarm'];
-		$UpdateTimeAlarm = date("Y-m-d H:i:s");
-		$UpdateBy = $_POST['UpdateBy'];
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		try {
+			if (empty($_POST['ControlNameAlarm'])) {
+				echo 'ControlNameAlarm empty';
+			} else if (empty($_POST['ControlNameAlarm'])) {
+				echo 'ControlNameAlarm empty';
+			} else if (empty($_POST['UpdateBy'])) {
+				echo 'UpdateBy empty';
+			} else {
+				$ControlNameAlarm = $_POST['ControlNameAlarm'];
+				$ControlStatusAlarm = $_POST['ControlStatusAlarm'];
+				$UpdateBy = $_POST['UpdateBy'];
+				
+				//Mendapatkan Nilai Variable
+				date_default_timezone_set('Asia/Jakarta');
+				$UpdateTimeAlarm = date("Y-m-d H:i:s");
 
-		//Pembuatan Syntax SQL
-		$sql = "INSERT INTO tbl_control (control_name,control_status,update_time,updated_by) VALUES ('$ControlNameAlarm','$ControlStatusAlarm','$UpdateTimeAlarm','$UpdateBy')";
-		//Import File Koneksi databaset
-		require_once('koneksi.php');
-		//Eksekusi Query database
-		if(pg_query($con,$sql))
-		{
-			echo 'Success';
+				//Pembuatan Syntax SQL
+				$sql = "INSERT INTO tbl_control (control_name,control_status,update_time,updated_by) VALUES ('$ControlNameAlarm','$ControlStatusAlarm','$UpdateTimeAlarm','$UpdateBy')";
+				//Import File Koneksi databaset
+				require_once('koneksi.php');
+				//Eksekusi Query database
+				if(pg_query($con,$sql))
+				{
+					echo 'Success';
+				}
+				else
+				{
+					echo 'Failed';
+				}
+				pg_close($con);
+			}
+		} catch (Exception $e) {
+			echo 'Caught exception: ',  $e, "\n";
 		}
-		else
-		{
-			echo 'Failed';
-		}
-		pg_close($con);
 	}
 ?>
