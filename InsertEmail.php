e64a3cc261a43299c20d35ff94506b2212594fd0
dicky.akbar@dwidasa.com <akbar128256512>
2020-02-08 17:25:28 +0700

    com

--- InsertEmail.php
+++ InsertEmail.php
@@ -1,16 +1,16 @@
 <?php 
 	if($_SERVER['REQUEST_METHOD']=='POST'){
-		//Mendapatkan Nilai Variable
-		date_default_timezone_set('Asia/Jakarta');
-		
 		if (isset($_POST['EmailAddress'])) {
 			echo 'Email Address empty';
 		} else if (isset($_POST['UpdateBy'])) {
 			echo 'UpdateBy empty';
 		} else {
-			$EmailAddress = $_POST["EmailAddress"];
+			$EmailAddress = $_POST['EmailAddress'];
+			$UpdateBy = $_POST['UpdateBy'];
+			
+			//Mendapatkan Nilai Variable
+			date_default_timezone_set('Asia/Jakarta');
 			$UpdateTimeEmail = date("Y-m-d H:i:s");
-			$UpdateBy = $_POST["UpdateBy"];
 
 			//Pembuatan Syntax SQL
 			$sql = "INSERT INTO tbl_email (email_address,update_time,updated_by) VALUES ('$EmailAddress','$UpdateTimeEmail','$UpdateBy')";
