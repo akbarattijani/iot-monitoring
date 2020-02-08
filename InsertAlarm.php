dda66ea807a4ca1a64467fc95a5d5051318a295f
dicky.akbar@dwidasa.com <akbar128256512>
2020-02-08 18:23:42 +0700

    com

--- InsertAlarm.php
+++ InsertAlarm.php
@@ -1,5 +1,5 @@
 <?php 
-	if($_SERVER['REQUEST_METHOD']=='POST'){
+	if($_SERVER['REQUEST_METHOD']=='POST') {
 		try {
 			if (empty($_POST['ControlNameAlarm'])) {
 				echo 'ControlNameAlarm empty';
--- InsertEngine.php
+++ InsertEngine.php
@@ -1,27 +1,38 @@
 <?php 
-	if($_SERVER['REQUEST_METHOD']=='POST')
-	{
-		//Mendapatkan Nilai Variable
-		date_default_timezone_set('Asia/Jakarta');
-		
-		$ControlNameEngine = $_POST['ControlNameEngine'];
-		$ControlStatusEngine = $_POST['ControlStatusEngine'];
-		$UpdateTimeEngine = date("Y-m-d H:i:s");
-		$UpdateBy = $_POST['UpdateBy'];
-		
-		//Pembuatan Syntax SQL
-		$sql = "INSERT INTO tbl_control (control_name,control_status,update_time,updated_by) VALUES ('$ControlNameEngine','$ControlStatusEngine','$UpdateTimeEngine','$UpdateBy')";
-		//Import File Koneksi databaset
-		require_once('koneksi.php');
-		//Eksekusi Query database
-		if(pg_query($con,$sql))
-		{
-			echo 'Success';
+	if($_SERVER['REQUEST_METHOD']=='POST') {
+		try {
+			if (empty($_POST['ControlNameEngine'])) {
+				echo 'ControlNameEngine empty';
+			} else if (empty($_POST['ControlStatusEngine'])) {
+				echo 'ControlStatusEngine empty';
+			} else if (empty($_POST['UpdateBy'])) {
+				echo 'UpdateBy empty';
+			} else {
+				$ControlNameEngine = $_POST['ControlNameEngine'];
+				$ControlStatusEngine = $_POST['ControlStatusEngine'];
+				$UpdateBy = $_POST['UpdateBy'];
+				
+				//Mendapatkan Nilai Variable
+				date_default_timezone_set('Asia/Jakarta');
+				$UpdateTimeEngine = date("Y-m-d H:i:s");
+				
+				//Pembuatan Syntax SQL
+				$sql = "INSERT INTO tbl_control (control_name,control_status,update_time,updated_by) VALUES ('$ControlNameEngine','$ControlStatusEngine','$UpdateTimeEngine','$UpdateBy')";
+				//Import File Koneksi databaset
+				require_once('koneksi.php');
+				//Eksekusi Query database
+				if(pg_query($con,$sql))
+				{
+					echo 'Success';
+				}
+				else
+				{
+					echo 'Failed';
+				}
+				pg_close($con);
+			}
+		} catch (Exception $e) {
+			echo 'Caught exception: ',  $e, "\n";
 		}
-		else
-		{
-			echo 'Failed';
-		}
-		pg_close($con);
 	}
 ?>
