731b0a6cac44fe087d2d2fd9d820b02d55bbfd14
dicky.akbar@dwidasa.com <akbar128256512>
2020-02-08 19:35:34 +0700

    com

--- InsertJarak.php
+++ InsertJarak.php
@@ -1,25 +1,28 @@
 <?php 
-	if($_SERVER['REQUEST_METHOD']=='POST')
-	{
-		//Mendapatkan Nilai Variable
-		date_default_timezone_set('Asia/Jakarta');
-		
-		$Distance = $_POST['Distance'];
-		$UpdateTime = date("Y-m-d H:i:s");
+	if($_SERVER['REQUEST_METHOD']=='POST') {
+		if (empty($_POST['Distance'])) {
+			echo 'Distance empty';
+		} else {
+			//Mendapatkan Nilai Variable
+			date_default_timezone_set('Asia/Jakarta');
+			
+			$Distance = $_POST['Distance'];
+			$UpdateTime = date("Y-m-d H:i:s");
 
-		//Pembuatan Syntax SQL
-		$sql = "INSERT INTO tbl_jarak (distance,update_time) VALUES ('$Distance','$UpdateTime')";
-		//Import File Koneksi databaset
-		require_once('koneksi.php');
-		//Eksekusi Query database
-		if(pg_query($con,$sql))
-		{
-			echo 'Success';
+			//Pembuatan Syntax SQL
+			$sql = "INSERT INTO tbl_jarak (distance,update_time) VALUES ('$Distance','$UpdateTime')";
+			//Import File Koneksi databaset
+			require_once('koneksi.php');
+			//Eksekusi Query database
+			if(pg_query($con,$sql))
+			{
+				echo 'Success';
+			}
+			else
+			{
+				echo 'Failed';
+			}
+			pg_close($con);
 		}
-		else
-		{
-			echo 'Failed';
-		}
-		pg_close($con);
 	}
 ?>
