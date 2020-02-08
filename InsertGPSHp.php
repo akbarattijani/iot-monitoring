145b4c3193464bcf2043c4966415e972549664e4
dicky.akbar@dwidasa.com <akbar128256512>
2020-02-08 18:59:44 +0700

    com

--- InsertGPSHp.php
+++ InsertGPSHp.php
@@ -1,26 +1,31 @@
 <?php 
-	if($_SERVER['REQUEST_METHOD']=='POST')
-	{
-		//Mendapatkan Nilai Variable
-		date_default_timezone_set('Asia/Jakarta');
-		
-		$GpsCoordinatHP = $_POST['GpsCoordinatHP'];
-		$UpdateTime = date("Y-m-d H:i:s");
-		$UpdateBy = date("UpdateBy");
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		if (empty($_POST['GpsCoordinatHP'])) {
+			echo 'GpsCoordinatHP empty';
+		} else if (empty($_POST['UpdateBy'])) {
+			echo 'UpdateBy empty';
+		} else {
+			//Mendapatkan Nilai Variable
+			date_default_timezone_set('Asia/Jakarta');
+			
+			$GpsCoordinatHP = $_POST['GpsCoordinatHP'];
+			$UpdateTime = date("Y-m-d H:i:s");
+			$UpdateBy = $_POST['UpdateBy'];
 
-		//Pembuatan Syntax SQL
-		$sql = "INSERT INTO tbl_gps_hp (gps_coordinat_hp,update_time,update_by) VALUES ('$GpsCoordinatHP','$UpdateTime','$UpdateBy')";
-		//Import File Koneksi databaset
-		require_once('koneksi.php');
-		//Eksekusi Query database
-		if(pg_query($con,$sql))
-		{
-			echo 'Success';
+			//Pembuatan Syntax SQL
+			$sql = "INSERT INTO tbl_gps_hp (gps_coordinat_hp,update_time,update_by) VALUES ('$GpsCoordinatHP','$UpdateTime','$UpdateBy')";
+			//Import File Koneksi databaset
+			require_once('koneksi.php');
+			//Eksekusi Query database
+			if(pg_query($con,$sql))
+			{
+				echo 'Success';
+			}
+			else
+			{
+				echo 'Failed';
+			}
+			pg_close($con);
 		}
-		else
-		{
-			echo 'Failed';
-		}
-		pg_close($con);
 	}
 ?>
