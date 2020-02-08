8c54d4ce3e32459f9b12bbfa28a594e88c99059a
dicky.akbar@dwidasa.com <akbar128256512>
2020-02-08 17:16:00 +0700

    com

--- InsertEmail.php
+++ InsertEmail.php
@@ -1,27 +1,31 @@
 <?php 
-	if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['EmailAddress']) && isset($_POST['UpdateBy']){
+	if($_SERVER['REQUEST_METHOD']=='POST'){
 		//Mendapatkan Nilai Variable
 		date_default_timezone_set('Asia/Jakarta');
 		
-		$EmailAddress = $_POST['EmailAddress'];
-		$UpdateTimeEmail = date("Y-m-d H:i:s");
-		$UpdateBy = $_POST['UpdateBy'];
+		if (isset($_POST['EmailAddress'])) {
+			echo 'Email Address empty';
+		} else if (isset($_POST['UpdateBy'])) {
+			echo 'UpdateBy empty';
+		} else {
+			$EmailAddress = $_POST['EmailAddress'];
+			$UpdateTimeEmail = date("Y-m-d H:i:s");
+			$UpdateBy = $_POST['UpdateBy'];
 
-		//Pembuatan Syntax SQL
-		$sql = "INSERT INTO tbl_email (email_address,update_time,updated_by) VALUES ('$EmailAddress','$UpdateTimeEmail','$UpdateBy')";
-		//Import File Koneksi databaset
-		require_once('koneksi.php');
-		//Eksekusi Query database
-		if(pg_query($con,$sql))
-		{
-			echo 'Success';
+			//Pembuatan Syntax SQL
+			$sql = "INSERT INTO tbl_email (email_address,update_time,updated_by) VALUES ('$EmailAddress','$UpdateTimeEmail','$UpdateBy')";
+			//Import File Koneksi databaset
+			require_once('koneksi.php');
+			//Eksekusi Query database
+			if(pg_query($con,$sql))
+			{
+				echo 'Success';
+			}
+			else
+			{
+				echo 'Failed';
+			}
+			pg_close($con);
 		}
-		else
-		{
-			echo 'Failed';
-		}
-		pg_close($con);
-	} else {
-		echo 'Failed Insert';
 	}
 ?>
